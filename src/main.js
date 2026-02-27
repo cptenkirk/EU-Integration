c4cfea92e4d4797d3c5178d3ec17f0fa18b114e4
cptenkirk <cptenimran@gmail.com>
2026-02-27 12:56:54 +0100

    Update Leaflet icon URLs and remove default icon URL

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+    iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+    shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+});
 import { getAIResponseForCountry } from "./aiprompt.js";
 import { europa } from './europa.js';
 import { initializeMap, openCountryInfoDialog, closeCountryInfoDialog, updateMapLabels } from './maplayout.js';
